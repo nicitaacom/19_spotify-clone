b1dce8447bcc3b7fd5f2aaf2c08064dac1b9e143
Nicitaa <39565703+Nicitaa@users.noreply.github.com>
2023-05-31 11:28:45 +0000

    44:39

--- components/Header.tsx
+++ components/Header.tsx
@@ -49,8 +49,11 @@ const handleLogout = () => {
         <div className='flex justify-between items-center gap-x-4'>
           <>
             <div>
-              <Button>
+              <Button className="bg-transparent text-neutral-300 font-medium">
                 Sign up
+              </Button>
+              <Button className="bg-white px-6 py-2">
+                Log in
               </Button>
             </div>
           </>
