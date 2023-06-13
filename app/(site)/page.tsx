e8a22c94e8183a303212705869e9ff6178b01588
Nicitaa <39565703+Nicitaa@users.noreply.github.com>
2023-06-13 20:46:19 +0000

    52-14

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -4,7 +4,12 @@ export default function Home() {
   return (
     <div className="text-neutral-400 rounded-lg w-full h-full overflow-hidden overflow-y-auto">
       <Header>
-        Header
+        <div className='mb-2'>
+          <h1 className='text-white text-3xl font-semibold'>Welcome back</h1>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4"> 
+
+        </div>
       </Header>
     </div>
     )
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,6 +4,9 @@ import { useRouter } from "next/navigation";
 import { twMerge } from "tailwind-merge";
 
 import { RxCaretLeft, RxCaretRight } from 'react-icons/rx'
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+import Button from "./Button";
 
 interface HeaderProps {
   children:React.ReactNode
@@ -19,12 +22,12 @@ const Header:React.FC<HeaderProps> = ({children,className}) => {
   }
 
   return ( 
-    <div className={twMerge(`h-fit bg-gradient-to-b from-emerald-600 p-6`,className)}>
-      <div className="w-full mb-4 flex justify-center items-center">
+    <div className={twMerge(`h-fit bg-gradient-to-b from-emerald-800 p-6`,className)}>
+      <div className="w-full mb-4 flex justify-between items-center">
         <div className="hidden md:flex gap-x-2 items-center">
           <button className="rounded-full bg-black flex justify-center items-center hover:opacity-75 transition"
           onClick={() => router.back()}>
-            <RxCaretLeft className="text-white" size={35}/>
+            <RxCaretLeft className="text-white" size={35} />
           </button>
           <button className="rounded-full bg-black flex justify-center items-center hover:opacity-75 transition"
           onClick={() => router.forward()}>
@@ -32,9 +35,29 @@ const Header:React.FC<HeaderProps> = ({children,className}) => {
           </button>
         </div>
         <div className='flex md:hidden gap-x-2 items-center'>
-
+          <button className="rounded-full p-2 bg-white flex items-center jusitfy-center hover:opacity-75 transition">
+            <HiHome className="text-black" size={20}/>
+          </button>
+          <button className="rounded-full p-2 bg-white flex items-center jusitfy-center hover:opacity-75 transition">
+            <BiSearch className="text-black" size={20}/>
+          </button>
+        </div>
+        <div className="flex justufy-between items-center gap-x-4">
+          <>
+            <div>
+              <Button className="bg-transparent text-neutral-300 font-medium" onClick={() => {}}>
+                Sign up
+              </Button>
+            </div>
+            <div>
+              <Button className="bg-white px-6 py-2" onClick={() => {}}>
+                Log in
+              </Button>
+            </div>
+          </>
         </div>
       </div>
+      {children}
     </div>
    );
 }
