7e86cd9db989cabf5748edb76ad517a81953511e
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-10-29 19:43:42 +0100

    5:58:21

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"BB4pg7n8Lru4+UTKOYz82TzT4tRorNC3HJnDF6jJd8E=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"o0etiXyXDrGbLT1kdN7h6BRnicmhumwFQ/AzuTFAy6Q=\"\n}"
---
+++ libs/stripe.ts
@@ -0,0 +1,12 @@
+import Stripe from "stripe";
+
+export const stripe = new Stripe(
+  process.env.STRIPE_SECRET_KEY ?? '',
+  {
+    apiVersion:'2023-10-16',
+    appInfo:{
+      name:'19_spotify-clone',
+      version:'0.1.0'
+    }
+  }
+)
