e76e3297dd41c4387ef4d86895dfdcee1778968f
Nikita <nicitaacom@gmail.com>
2026-06-06 18:30:56 +0200

    upd: no utm stats dashboard

--- app/features/UTM/utm-stats/page.tsx
+++
@@ -1,9 +0,0 @@
-import { selectDBUTMStatsAction } from "./actions/selectDBUTMStatsAction"
-import { UTMDashboard } from "./components/UTMDashboard"
-
-export default async function UTMStatsPage() {
-  const utmStatsResponse = await selectDBUTMStatsAction()
-
-  if (typeof utmStatsResponse === "string") return <h1 className="text-danger text-2xl">{utmStatsResponse}</h1>
-  else return <UTMDashboard utmStatsResponse={utmStatsResponse} />
-}
