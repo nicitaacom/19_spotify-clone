064d5837fc0f0e21d5dffa4a16f42312e84cb99c
Nikita <nicitaacom@gmail.com>
2026-08-03 23:28:43 +0200

    chore: organized envs
    sported and commented and removed stuff I don't need

--- env.d.ts
+++ env.d.ts
@@ -2,35 +2,22 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       NEXT_PUBLIC_PRODUCTION_URL: string
-      NEXT_PUBLIC_SITE_URL: string
-      NEXT_PUBLIC_VERCEL_URL: string
 
       NEXT_PUBLIC_SUPABASE_URL: string
-      // read internally by @supabase/auth-helpers-nextjs, never an explicit process.env reference in this repo's own source
-      // eslint-disable-next-line local-rules/no-defined-unused-envs
       NEXT_PUBLIC_SUPABASE_ANON_KEY: string
       SUPABASE_SERVICE_ROLE_KEY: string
 
       UPSTASH_REDIS_REST_URL: string
       UPSTASH_REDIS_REST_TOKEN: string
-      UPSTASH_REDIS_URL: string
 
       PUSHER_APP_ID: string
       NEXT_PUBLIC_PUSHER_APP_KEY: string
       PUSHER_SECRET: string
 
-      GITHUB_CLIENT_ID: string
-      GITHUB_CLIENT_SECRET: string
-      NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY_TEST: string
-      NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY_LIVE: string
       STRIPE_SECRET_KEY_TEST: string
       STRIPE_SECRET_KEY_LIVE: string
       STRIPE_WEBHOOK_SECRET_TEST: string
       STRIPE_WEBHOOK_SECRET_LIVE: string
-
-      NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: string
-      STRIPE_SECRET_KEY: string
-      STRIPE_WEBHOOK_SECRET: string
 
       NEXT_PUBLIC_TURNSTILE_SITE_KEY: string
       TURNSTILE_SECRET_KEY: string
