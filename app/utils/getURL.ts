b96b53c7ab27b965b47637542dc99ad7bf91ca8b
Nikita <nicitaacom@gmail.com>
2026-08-03 23:29:01 +0200

    chore: eslint ignore SITE_URL VERCEL_URL

--- app/utils/getURL.ts
+++ app/utils/getURL.ts
@@ -4,8 +4,10 @@ export const getURL = (path = "") => {
     typeof window !== "undefined"
       ? window.location.origin
       : process.env.NODE_ENV === "development"
-        ? "http://localhost:3000"
-        : process.env.NEXT_PUBLIC_PRODUCTION_URL ?? process.env.NEXT_PUBLIC_SITE_URL ?? process.env.NEXT_PUBLIC_VERCEL_URL ?? ""
+        ? "http://localhost:3023"
+          // NEXT_PUBLIC_SITE_URL/NEXT_PUBLIC_VERCEL_URL are intentionally undeclared, vercel autofills these 2
+        : // eslint-disable-next-line local-rules/no-undefined-used-envs
+          (process.env.NEXT_PUBLIC_PRODUCTION_URL ?? process.env.NEXT_PUBLIC_SITE_URL ?? process.env.NEXT_PUBLIC_VERCEL_URL)
 
   if (!origin) {
     return ""
@@ -13,7 +15,8 @@ export const getURL = (path = "") => {
 
   const normalizedOrigin = origin.includes("http") ? origin : `https://${origin}`
   const normalizedPath = path.startsWith("/") ? path.slice(1) : path
-  const baseUrl = normalizedOrigin.charAt(normalizedOrigin.length - 1) === "/" ? normalizedOrigin : `${normalizedOrigin}/`
+  const baseUrl =
+    normalizedOrigin.charAt(normalizedOrigin.length - 1) === "/" ? normalizedOrigin : `${normalizedOrigin}/`
 
   return normalizedPath ? `${baseUrl}${normalizedPath}` : baseUrl
 }
